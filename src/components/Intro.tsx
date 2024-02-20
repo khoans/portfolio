e6d8082a0350fa0bfb53f998cd40f849f984095b
khoansfx <khoansfx20189@funix.edu.vn>
2024-02-20 14:55:21 +0700

    update profile

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -97,7 +97,7 @@ export default function Intro() {
         </a>
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/nskhoa1502/"
           target="_blank"
           rel="noopener noreferrer"
         >
